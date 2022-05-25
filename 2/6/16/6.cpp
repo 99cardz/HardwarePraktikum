8a2083aa3c7720129ac998793cf5b5ffbb73ae8b
99cardz <jonathan.schulze.de@gmail.com>
2022-05-25 13:33:37 +0200

    Add files via upload

---
+++ 2/6/16/6.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <b15f/b15f.h>
+#include <string>
+
+using namespace std;
+
+
+int fd;
+
+
+int open_port(void) {
+  int fd; /* File descriptor for the port */
+	struct termios options;
+
+  fd = open("/dev/ttyUSB1", O_RDWR | O_NOCTTY | O_NDELAY);
+  if (fd == -1)
+  {
+    cout<<"open_port: Unable to open \n";
+  }
+  else {
+  	// setting terminal options
+    fcntl(fd, F_SETFL, FNDELAY);
+		tcgetattr(fd, &options);
+		cfsetispeed(&options, B115200);
+		cfsetospeed(&options, B115200);
+		options.c_cflag |= (CLOCAL | CREAD);
+		options.c_cflag &= ~PARENB;
+		options.c_cflag &= ~CSTOPB;
+		options.c_cflag &= ~CSIZE;
+		options.c_cflag |= CS8;
+		tcsetattr(fd, TCSANOW, &options);
+  	return (fd);
+  }
+  return 0;
+}
+
+void writeAndMeasure(int f, B15F& drv) {
+	string str = string(":w21=5.\r\n:w23=") + to_string(f) + string("00,0.\r\n");
+	char write_buffer[32];
+	strcpy(write_buffer, str.c_str());
+	write(fd, write_buffer , 32);
+	int sum1 = 0;
+	for (int i = 0; i < (int) 100 / f; i++)
+		sum1 += drv.analogRead(1);
+	cout << f << "," << (int) sum1 / 20 << "," << drv.analogRead(0) << endl;
+}
+
+int main()
+{
+	
+	B15F& drv = B15F::getInstance();
+	drv.analogWrite0(0);
+	
+	fd = open_port();
+	
+	for (double f = 1; f < 1000000.0; f *= 1.1) 
+		writeAndMeasure((int) f, drv);
+	
+}
